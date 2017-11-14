e61ea9449b0cd6a46f36232cdae590eb9252ffaa
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-14 09:17:00 +0000

    Add function calculateDivisors + test

--- calculateDivisors/calculateDivisors.js
+++ calculateDivisors/calculateDivisors.js
@@ -1,8 +1,13 @@
 // Please do not change the name of this function
 var calculateDivisors = function (number) {
-  // Your code here
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0)
+      sum += i;
+  }
+  return sum;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {calculateDivisors};
+  module.exports = { calculateDivisors };
 }
--- calculateDivisors/spec/calculateDivisors.spec.js
+++ calculateDivisors/spec/calculateDivisors.spec.js
@@ -1,9 +1,13 @@
 const path = require('path');
-const {expect} = require('chai');
-const {calculateDivisors} = require(path.join(__dirname, '..', './calculateDivisors.js'));
+const { expect } = require('chai');
+const { calculateDivisors } = require(path.join(__dirname, '..', './calculateDivisors.js'));
 
 describe('calculateDivisors()', function () {
-  it('ADD FIRST TEST AND RENAME', function () {
-    
+  it('return sum of the all multiples of three and five below a given number', function () {
+    expect(calculateDivisors(1)).to.equal(0);
+    expect(calculateDivisors(5)).to.equal(3);
+    expect(calculateDivisors(6)).to.equal(8);
+    expect(calculateDivisors(10)).to.equal(23);
+    expect(calculateDivisors(12)).to.equal(33);
   });
 });
