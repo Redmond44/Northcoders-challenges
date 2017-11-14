e3ec0d07a35823c2e266185b8755a34a942d5501
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-14 10:02:10 +0000

    Works for words starting with few consonants at the beginning

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,19 +1,22 @@
 // Please do not change the name of this function
 var pigLatin = function (str) {
-  const vowels = ['a', 'e', 'i', 'o', 'u' ];
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
 
   if (vowels.includes(str[0])) return str + 'way';
+
   else {
-    str = str.slice(1) + str.slice(0, 1);
-    if (vowels.includes(str[0])) return str + 'ay';
+    const index = function(str) {
+      for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str.charAt(i))) {
+          return i;
+        }
+      }
+    };
+    if (index(str) === undefined) return str + 'ay';
+    return str.slice(index(str)) + str.slice(0, index(str)) + 'ay';
   }
-
-// indexOf
-// znajdz index pierwszej samogloski
-// split(' ') for sentence
-
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {pigLatin};
+  module.exports = { pigLatin };
 }
--- pigLatin/spec/pigLatin.spec.js
+++ pigLatin/spec/pigLatin.spec.js
@@ -9,18 +9,17 @@ describe('pigLatin()', function () {
     expect(pigLatin('a')).to.equal('away');
     expect(pigLatin('i')).to.equal('iway');
   });
-  // it('return consonant + "ay" for single consonant', function () {
-  //   expect(pigLatin('n')).to.equal('nay');
-  //   expect(pigLatin('b')).to.equal('bay');
-  // });
+  it('return consonant + "ay" for single consonant', function () {
+    expect(pigLatin('n')).to.equal('nay');
+    expect(pigLatin('b')).to.equal('bay');
+  });
   it('return str + "way" for string starts with a vowel', function () {
     expect(pigLatin('algorithm')).to.equal('algorithmway');
   });
   it('move starting consonant to the end and append "ay" for strings with one consonant at the beginning', function () {
     expect(pigLatin('northcoders')).to.equal('orthcodersnay');
   });
-  // it('move all starting consonants to the end and append "ay" for strings with few consonants at the beginning', function () {
-  //   expect(pigLatin('sheffield')).to.equal('effieldshay');
-  // });
-
+  it('move all starting consonants to the end and append "ay" for strings with few consonants at the beginning', function () {
+    expect(pigLatin('sheffield')).to.equal('effieldshay');
+  });
 });
