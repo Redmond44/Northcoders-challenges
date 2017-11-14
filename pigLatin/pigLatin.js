e49e5c0f71226c76362def33e463abe6703ccf9a
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-14 08:52:59 +0000

    Works for words starting with vowels

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,17 @@
 // Please do not change the name of this function
 var pigLatin = function (str) {
-  // Your code here
+  const vowels = ['a', 'e', 'i', 'o', 'u' ];
+
+  if (vowels.includes(str[0])) return str + 'way';
+  else {
+    str = str.slice(1) + str.slice(0, 1);
+    if (vowels.includes(str[0])) return str + 'ay';
+  }
+
+// indexOf
+// znajdz index pierwszej samogloski
+// split(' ') for sentence
+
 };
 
 if (typeof module !== 'undefined') {
--- pigLatin/spec/pigLatin.spec.js
+++ pigLatin/spec/pigLatin.spec.js
@@ -5,8 +5,22 @@ var {pigLatin} = require(path.join(__dirname, '..', './pigLatin.js'));
 
 describe('pigLatin()', function () {
   'use strict';
-
-  it('ADD TEST AND RENAME', function () {
-    
+  it('return vowel + "way" for single vowel', function () {
+    expect(pigLatin('a')).to.equal('away');
+    expect(pigLatin('i')).to.equal('iway');
   });
+  // it('return consonant + "ay" for single consonant', function () {
+  //   expect(pigLatin('n')).to.equal('nay');
+  //   expect(pigLatin('b')).to.equal('bay');
+  // });
+  it('return str + "way" for string starts with a vowel', function () {
+    expect(pigLatin('algorithm')).to.equal('algorithmway');
+  });
+  it('move starting consonant to the end and append "ay" for strings with one consonant at the beginning', function () {
+    expect(pigLatin('northcoders')).to.equal('orthcodersnay');
+  });
+  // it('move all starting consonants to the end and append "ay" for strings with few consonants at the beginning', function () {
+  //   expect(pigLatin('sheffield')).to.equal('effieldshay');
+  // });
+
 });
