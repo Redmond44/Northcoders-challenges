de2e08254ee7b498f2eab2676d980267c92409ad
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-16 08:51:43 +0000

    Return length and tags

--- getTweetData/getTweetData.js
+++ getTweetData/getTweetData.js
@@ -1,8 +1,33 @@
 // Please do not change the name of this function
-var getTweetData = function () {
+var getTweetData = function (tweet) {
+
+  let tweetData = {};
+
+  tweetData.length = tweet.length;  
+
+  tweetData.tagCount = 0;
+
+  tweetData.tags = tweet.split(' ').reduce((acc, word) => {
+    if (word[0] === '#') {
+      tweetData.tagCount += 1;
+      acc.push(word);
+    }
+    return acc;
+  }, []);
+
+  // tweetData.mentionCount = 0;
+  // tweetData.mentions = tweet.split(' ').reduce((acc, word) => {
+  //   if (word[0] === '@') {
+  //     tweetData.tagCount += 1;
+  //     return acc.push(word);git sta
+  //   }
+  //   return acc;
+  // }, []);
+  // console.log(tweetData);
+  return tweetData;
 
 };
 
-if ( typeof module !== 'undefined' ) {
-  module.exports = {getTweetData};
+if (typeof module !== 'undefined') {
+  module.exports = { getTweetData };
 }
--- getTweetData/spec/getTweetData.spec.js
+++ getTweetData/spec/getTweetData.spec.js
@@ -11,9 +11,25 @@ describe('getTweetData()', function () {
 
   });
 
-  it('takes a single tweet argument', function () {
-    expect(getTweetData.length).to.equal(1);
+  it('returns length of the tweet', function () {
+    expect(getTweetData('My awesome tweet').length).to.equal(16);
+    expect(getTweetData('My awesome tweet to @northcoders').length).to.equal(32);
+    expect(getTweetData('My awesome tweet about #coding').length).to.equal(30);
+    expect(getTweetData('My awesome tweet about #coding to @northcoders').length).to.equal(46);
+    expect(getTweetData('I am #coding with @northcoders I love #coding and @northcoders').length).to.equal(62);
   });
+
+  it('returns amount of hash tags', function () {    
+    expect(getTweetData('My awesome tweet').tagCount).to.equal(0);
+    expect(getTweetData('My awesome tweet to @northcoders').tagCount).to.equal(0);
+    expect(getTweetData('My awesome tweet about #coding').tagCount).to.equal(1);
+    expect(getTweetData('My awesome tweet about #coding to @northcoders').tagCount).to.equal(1);
+  });
+
+  // empty string
+  // single letter, word
+  // string with #
+  // string with @ 
 
 
 });
