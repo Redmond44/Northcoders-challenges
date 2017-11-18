1be57d647b4ea836745b57d3456bee3e10c99a5d
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-18 20:06:40 +0000

    Returns unique hash tags and mentions

--- getTweetData/getTweetData.js
+++ getTweetData/getTweetData.js
@@ -1,31 +1,22 @@
 // Please do not change the name of this function
 var getTweetData = function (tweet) {
-
   let tweetData = {};
-
   tweetData.length = tweet.length;  
+  tweetData.tags = [];  
+  tweetData.mentions = [];
 
-  tweetData.tagCount = 0;
-
-  tweetData.tags = tweet.split(' ').reduce((acc, word) => {
-    if (word[0] === '#') {
-      tweetData.tagCount += 1;
-      acc.push(word);
+  tweet.split(' ').forEach((word) => {
+    if (word[0] === '#' && !tweetData.tags.includes(word)) {
+      tweetData.tags.push(word);
     }
-    return acc;
-  }, []);
+    if (word[0] === '@' && !tweetData.mentions.includes(word)) {
+      tweetData.mentions.push(word);
+    }
+  });
+  tweetData.tagCount = tweetData.tags.length;
+  tweetData.mentionCount = tweetData.mentions.length;
 
-  // tweetData.mentionCount = 0;
-  // tweetData.mentions = tweet.split(' ').reduce((acc, word) => {
-  //   if (word[0] === '@') {
-  //     tweetData.tagCount += 1;
-  //     return acc.push(word);
-  //   }
-  //   return acc;
-  // }, []);
-  // console.log(tweetData);
   return tweetData;
-
 };
 
 if (typeof module !== 'undefined') {
--- getTweetData/spec/getTweetData.spec.js
+++ getTweetData/spec/getTweetData.spec.js
@@ -4,32 +4,33 @@ var expect = require('chai').expect;
 var {getTweetData} = require(path.join(__dirname, '..', './getTweetData.js'));
 
 describe('getTweetData()', function () {
-  'use strict';
-
   it('exists', function () {
     expect(getTweetData).to.be.a('function');
-
   });
-
+  it('returns falsely properties for an empty string', function () {
+    expect(getTweetData('')).to.eql({ tags: [], mentions: [], tagCount: 0, mentionCount: 0, length: 0 });
+  });  
   it('returns length of the tweet', function () {
     expect(getTweetData('My awesome tweet').length).to.equal(16);
     expect(getTweetData('My awesome tweet to @northcoders').length).to.equal(32);
-    expect(getTweetData('My awesome tweet about #coding').length).to.equal(30);
-    expect(getTweetData('My awesome tweet about #coding to @northcoders').length).to.equal(46);
-    expect(getTweetData('I am #coding with @northcoders I love #coding and @northcoders').length).to.equal(62);
   });
-
   it('returns amount of hash tags', function () {    
     expect(getTweetData('My awesome tweet').tagCount).to.equal(0);
-    expect(getTweetData('My awesome tweet to @northcoders').tagCount).to.equal(0);
     expect(getTweetData('My awesome tweet about #coding').tagCount).to.equal(1);
-    expect(getTweetData('My awesome tweet about #coding to @northcoders').tagCount).to.equal(1);
   });
-
-  // empty string
-  // single letter, word
-  // string with #
-  // string with @ 
-
-
+  it('returns an array of hash tags', function () {    
+    expect(getTweetData('My awesome tweet').tags).to.eql([]);
+    expect(getTweetData('My awesome tweet about #coding').tags).to.eql(['#coding']);
+  });
+  it('returns amount of mentions', function () {    
+    expect(getTweetData('My awesome tweet to @northcoders').mentionCount).to.equal(1);
+    expect(getTweetData('My awesome tweet about #coding to @northcoders').mentionCount).to.equal(1);
+  });
+  it('returns an array of mentions', function () {    
+    expect(getTweetData('My awesome tweet to @northcoders').mentions).to.eql(['@northcoders']);
+    expect(getTweetData('My awesome tweet about #coding to @northcoders').mentions).to.eql(['@northcoders']);
+  });
+  it('returns only unique hash tags and handles mentioned', function () {    
+    expect(getTweetData('I am #coding with @northcoders I love #coding and @northcoders')).to.eql({ tags: ['#coding'], mentions: ['@northcoders'], tagCount: 1, mentionCount: 1, length: 62 });
+  });
 });
