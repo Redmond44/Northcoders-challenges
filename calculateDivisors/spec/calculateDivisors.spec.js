7fa5eab0796f767ecea74173372d68f6972fac7e
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-18 16:01:46 +0000

    Add more tests

--- calculateDivisors/spec/calculateDivisors.spec.js
+++ calculateDivisors/spec/calculateDivisors.spec.js
@@ -3,9 +3,14 @@ const { expect } = require('chai');
 const { calculateDivisors } = require(path.join(__dirname, '..', './calculateDivisors.js'));
 
 describe('calculateDivisors()', function () {
-  it('return sum of the all multiples of three and five below a given number', function () {
+  it('return 0 for numbers less than 4', function () {
     expect(calculateDivisors(1)).to.equal(0);
+    expect(calculateDivisors(3)).to.equal(0);
+  });
+  it('return 3 for numbers less than 6', function () {
     expect(calculateDivisors(5)).to.equal(3);
+  });
+  it('return sum of the all multiples of three and five below a given number', function () {
     expect(calculateDivisors(6)).to.equal(8);
     expect(calculateDivisors(10)).to.equal(23);
     expect(calculateDivisors(12)).to.equal(33);
