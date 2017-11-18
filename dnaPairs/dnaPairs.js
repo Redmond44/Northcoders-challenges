b00f83732440cba77019ae32b488e5bd642e93cf
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-18 20:23:04 +0000

    Add more tests

--- dnaPairs/dnaPairs.js
+++ dnaPairs/dnaPairs.js
@@ -1,8 +1,7 @@
-// Please do not change the name of this function
 var dnaPairs = function (dna) {
-  const pairs = {A: ['A', 'T'], T: ['T', 'A'], C: ['C', 'G'], G: ['G', 'C']};
+  const pairs = { A: ['A', 'T'], T: ['T', 'A'], C: ['C', 'G'], G: ['G', 'C'] };
   let dnaPairs = [];
-  
+
   dna.toUpperCase().split('').forEach((letter) => {
     dnaPairs.push(pairs[letter]);
   });
--- dnaPairs/spec/dnaPairs.spec.js
+++ dnaPairs/spec/dnaPairs.spec.js
@@ -1,22 +1,26 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {dnaPairs} = require(path.join(__dirname, '..', './dnaPairs.js'));
+var { dnaPairs } = require(path.join(__dirname, '..', './dnaPairs.js'));
 
 describe('dnaPairs()', function () {
   'use strict';
-  it('returns an empty array for an empty string', function() {
+  it('returns an empty array for an empty string', function () {
     expect(dnaPairs('')).to.eql([]);
   });
-  it('returns nested array for one-letter string', function() {
+  it('returns nested array for one-letter string', function () {
     expect(dnaPairs('A')).to.eql([['A', 'T']]);
   });
-  it('matches each base with its pair, returning a nested array', function() {
+  it('matches each base with its pair, returning a nested array', function () {
     expect(dnaPairs('AG')).to.eql([['A', 'T'], ['G', 'C']]);
     expect(dnaPairs('ATAG')).to.eql([['A', 'T'], ['T', 'A'], ['A', 'T'], ['G', 'C']]);
   });
-  it('works for lower case', function() {
+  it('works for lower case', function () {
     expect(dnaPairs('ag')).to.eql([['A', 'T'], ['G', 'C']]);
     expect(dnaPairs('atag')).to.eql([['A', 'T'], ['T', 'A'], ['A', 'T'], ['G', 'C']]);
+  });
+  it('returns undefined if invalid letter', function () {
+    expect(dnaPairs('h')).to.eql([undefined]);
+    expect(dnaPairs('azg')).to.eql([['A', 'T'], undefined, ['G', 'C']]);
   });
 });
