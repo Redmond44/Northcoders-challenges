97bc1c82afde991e08852fe1236e91173c3ead1e
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-20 09:47:39 +0000

    Add herdTheBabies function + tests

--- herdTheBabies/herdTheBabies.js
+++ herdTheBabies/herdTheBabies.js
@@ -1,7 +1,17 @@
-// Please do not change the name of this function
+function herdTheBabies(str) {
+  let arrOfParentsWithBabies = [];
 
-function herdTheBabies () {
-
+  str.split('').sort().forEach((letter) => {
+    if (letter.toUpperCase() === letter) arrOfParentsWithBabies.push([letter]);
+    else {
+      arrOfParentsWithBabies.forEach((capitalLetter) => {
+        if (letter.toUpperCase() === capitalLetter[0]) capitalLetter.push(letter);
+      });
+    }
+  });
+  return arrOfParentsWithBabies.reduce((acc, pair) => {
+    return acc.concat(pair);
+  }, []).join('');
 }
 
-module.exports = {herdTheBabies};
+module.exports = { herdTheBabies };
--- herdTheBabies/spec/herdTheBabies.spec.js
+++ herdTheBabies/spec/herdTheBabies.spec.js
@@ -2,7 +2,14 @@ const expect = require('chai').expect;
 const {herdTheBabies} = require('../herdTheBabies');
 
 describe('herdTheBabies()', function () {
-  it('ADD TEST AND RENAME', function () {
-    
+  it('returns the same letter for a single letter', function () {
+    expect(herdTheBabies('B')).to.equal('B');
+  });
+  it('returns "parent" letter before its "baby" for one pair', function () {
+    expect(herdTheBabies('aA')).to.equal('Aa');
+  });
+  it('returns "parent" letter before its "baby" for several pairs', function () {
+    expect(herdTheBabies('aBA')).to.equal('AaB');
+    expect(herdTheBabies('bbaBccAC')).to.equal('AaBbbCcc');
   });
 });
