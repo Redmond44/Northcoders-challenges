5526b3ffaf9dff8ced433f6725708df7f7b05c40
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-21 13:38:27 +0000

    Nicer solution + new test

--- herdTheBabies/herdTheBabies.js
+++ herdTheBabies/herdTheBabies.js
@@ -1,17 +1,31 @@
 function herdTheBabies(str) {
-  let arrOfParentsWithBabies = [];
+  // let arrOfParentsWithBabies = [];
 
-  str.split('').sort().forEach((letter) => {
-    if (letter.toUpperCase() === letter) arrOfParentsWithBabies.push([letter]);
-    else {
-      arrOfParentsWithBabies.forEach((capitalLetter) => {
-        if (letter.toUpperCase() === capitalLetter[0]) capitalLetter.push(letter);
-      });
+  // str.split('').sort().forEach((letter) => {
+  //   if (letter.toUpperCase() === letter) arrOfParentsWithBabies.push([letter]);
+  //   else {
+  //     arrOfParentsWithBabies.forEach((capitalLetter) => {
+  //       console.log(capitalLetter);
+  //       if (letter.toUpperCase() === capitalLetter[0]) capitalLetter.push(letter);
+  //     });
+  //   }
+  // });
+  // return arrOfParentsWithBabies.reduce((acc, pair) => {
+  //   return acc.concat(pair);
+  // }, []).join('');
+  const findUpperCases = /[A-Z]/g;
+  const findLowerCases = /[a-z]/g;
+  const upperCases = str.match(findUpperCases).sort();
+  let lowerCases = str.match(findLowerCases);
+  if (lowerCases === null) lowerCases = '';
+
+  return upperCases.map((capitalLetter) => {
+    for (let i = 0; i < lowerCases.length; i++) {
+      if (lowerCases[i].toUpperCase() === capitalLetter[0])
+        capitalLetter += lowerCases[i];
     }
-  });
-  return arrOfParentsWithBabies.reduce((acc, pair) => {
-    return acc.concat(pair);
-  }, []).join('');
+    return capitalLetter;
+  }).join('');
 }
 
 module.exports = { herdTheBabies };
--- herdTheBabies/spec/herdTheBabies.spec.js
+++ herdTheBabies/spec/herdTheBabies.spec.js
@@ -2,13 +2,17 @@ const expect = require('chai').expect;
 const {herdTheBabies} = require('../herdTheBabies');
 
 describe('herdTheBabies()', function () {
-  it('returns the same letter for a single letter', function () {
+  it('returns the same letter for a single letter "parent"', function () {
     expect(herdTheBabies('B')).to.equal('B');
   });
   it('returns "parent" letter before its "baby" for one pair', function () {
     expect(herdTheBabies('aA')).to.equal('Aa');
   });
   it('returns "parent" letter before its "baby" for several pairs', function () {
+    expect(herdTheBabies('aAB')).to.equal('AaB');
+    expect(herdTheBabies('aAaaabbbbB')).to.equal('AaaaaBbbbb');
+  });
+  it('returns string that is sorted alphabetically', function () {
     expect(herdTheBabies('aBA')).to.equal('AaB');
     expect(herdTheBabies('bbaBccAC')).to.equal('AaBbbCcc');
   });
