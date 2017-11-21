9c5ee6306d222a853494a8d35f22528998d53772
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-21 09:37:29 +0000

    Add function sentenceToCamelCase + tests

--- sentenceToCamelCase/sentenceToCamelCase.js
+++ sentenceToCamelCase/sentenceToCamelCase.js
@@ -1,8 +1,10 @@
-// Please do not change the name of this function
-var sentenceToCamelCase = function () {
-  // your code goes here
+var sentenceToCamelCase = function (str, isUpperCase) {
+  return str.split(' ').map((word, index) => {
+    if (!isUpperCase && index === 0) return word[0].toLowerCase() + word.slice(1);
+    return word[0].toUpperCase() + word.slice(1);
+  }).join('');
 };
 
-if ( typeof module !== 'undefined' ) {
-  module.exports = {sentenceToCamelCase};
+if (typeof module !== 'undefined') {
+  module.exports = { sentenceToCamelCase };
 }
--- sentenceToCamelCase/spec/sentenceToCamelCase.spec.js
+++ sentenceToCamelCase/spec/sentenceToCamelCase.spec.js
@@ -4,11 +4,24 @@ var expect = require('chai').expect;
 var {sentenceToCamelCase} = require(path.join(__dirname, '..', './sentenceToCamelCase.js'));
 
 describe('sentenceToCamelCase()', function () {
-  'use strict';
-
-  it('ADD TEST AND RENAME', function () {
-    
+  it('returns the same word for single word input', function () {
+    expect(sentenceToCamelCase('this')).to.equal('this');
   });
-
-  // Add more assertions here
+  it('returns the UpperCamelCase word if second argument is true for single word input', function () {
+    expect(sentenceToCamelCase('this', true)).to.equal('This');
+    expect(sentenceToCamelCase('Book', true)).to.equal('Book');
+  });
+  it('returns the lowerCamelCase word if second argument is false for single word input', function () {
+    expect(sentenceToCamelCase('this', false)).to.equal('this');
+    expect(sentenceToCamelCase('Book', false)).to.equal('book');
+  });
+  it('returns only the first lowerCamelCase word if second argument is false for sentences', function () {
+    expect(sentenceToCamelCase('this sentence', false)).to.equal('thisSentence');
+    expect(sentenceToCamelCase('This sentence', false)).to.equal('thisSentence');
+  });
+  it('returns all UpperCamelCase words if second argument is true for sentences', function () {
+    expect(sentenceToCamelCase('this sentence', true)).to.equal('ThisSentence');
+    expect(sentenceToCamelCase('This sentence', true)).to.equal('ThisSentence');
+    expect(sentenceToCamelCase('This Bigger strange Sentence', true)).to.equal('ThisBiggerStrangeSentence');
+  });  
 });
