fb84a057d93fa79a6a72fb06cd88cfd0ed13107f
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-23 09:03:02 +0000

    Add function tillAddition + tests

--- tillAddition/spec/tillAddition.spec.js
+++ tillAddition/spec/tillAddition.spec.js
@@ -1,13 +1,25 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {tillAddition} = require(path.join(__dirname, '..', './tillAddition.js'));
+var { tillAddition } = require(path.join(__dirname, '..', './tillAddition.js'));
 
 describe('tillAddition()', function () {
-  'use strict';
-
-  it('RENAME AND ADD TEST', function () {
-    
+  it('works for one piece', function () {
+    expect(tillAddition({ '2p': 1 })).to.equal('£0.02');
   });
-  // Add more assertions here
+  it('works for few pieces', function () {
+    expect(tillAddition({ '1p': 1, '2p': 1, '5p': 1, '10p': 1, '20p': 1 })).to.equal('£0.38');
+  });
+  it('works for one coin or note', function () {
+    expect(tillAddition({ '£10': 1 })).to.equal('£10.00');
+  });
+  it('works for few coins and notes', function () {
+    expect(tillAddition({ '£1': 1, '£5': 1, '£10': 1, '£20': 1, '£50': 1 })).to.equal('£86.00');
+  });
+  it('works for mix of pieces, coins and notes', function () {
+    expect(tillAddition({'5p': 1,'10p': 1,'20p': 1,'50p': 1,'£1': 1})).to.equal('£1.85');
+  });
+  it('works for mix of amounts', function () {
+    expect(tillAddition({'5p': 5,'10p': 18,'20p': 2,'50p': 34,'£1': 100})).to.equal('£119.45');
+  });
 });
--- tillAddition/tillAddition.js
+++ tillAddition/tillAddition.js
@@ -1,8 +1,14 @@
 // Please do not change the name of this function
-var tillAddition = function () {
-  // your code goes here
+var tillAddition = function (till) {
+  let result = 0;
+  for (let prop in till) {
+    let value = prop.match(/\d+/);
+    if (prop.includes('p')) result += value[0] * till[prop] / 100;
+    if (prop.includes('£')) result += value[0] * till[prop];
+  }
+  return `£${result.toFixed(2)}`;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {tillAddition};
+  module.exports = { tillAddition };
 }
