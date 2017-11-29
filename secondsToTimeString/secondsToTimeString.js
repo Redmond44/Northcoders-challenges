b25bea6402f16c4cc44bf2dd185409c57372298e
dzewelina <ewelina.zukowska26@gmail.com>
2017-11-29 09:03:27 +0000

    Add secondsToTimeString function + tests

--- secondsToTimeString/secondsToTimeString.js
+++ secondsToTimeString/secondsToTimeString.js
@@ -1,8 +1,66 @@
-// Please do not change the name of this function
-function secondsToTimeString(seconds) {
+// function secondsToTimeString(seconds) {
 
-}
+//   let minutes = Math.floor(seconds / 60);
+//   let hours = Math.floor(minutes / 60);
+//   let days = Math.floor(hours / 24);
+//   let years = Math.floor(days / 365);
+//   days = days % 365;
+//   hours = hours % 24;
+//   minutes = minutes % 60;
+//   seconds = seconds % 60;
 
-if ( typeof module !== 'undefined' ) {
-  module.exports = {secondsToTimeString};
+//   let elementsArr = [];
+
+//   if (years === 1) elementsArr.push(`${years} year`);
+//   else if (years) elementsArr.push(`${years} years`);
+
+//   if (days === 1) elementsArr.push(`${days} day`);
+//   else if (days) elementsArr.push(`${days} days`);
+
+//   if (hours === 1) elementsArr.push(`${hours} hour`);
+//   else if (hours) elementsArr.push(`${hours} hours`);
+
+//   if (minutes === 1) elementsArr.push(`${minutes} minute`);
+//   else if (minutes) elementsArr.push(`${minutes} minutes`);
+
+
+//   if (seconds === 1) elementsArr.push(`${seconds} second`);
+//   else if (seconds) elementsArr.push(`${seconds} seconds`);
+
+//   if (!elementsArr.length) return `${seconds} seconds`;
+//   let finalStr =  elementsArr.join(', ');
+
+//   return finalStr.replace(/,(?=[^,]*$)/, ' and');
+// }
+
+function secondsToTimeString(seconds) {  
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+    let years = Math.floor(days / 365);
+    days = days % 365;
+    hours = hours % 24;
+    minutes = minutes % 60;
+    seconds = seconds % 60;
+  
+    const reference = {0: 'second', 1: 'minute', 2: 'hour', 3: 'day', 4: 'year'};
+    const elementsArr = [seconds, minutes, hours, days, years];
+
+    const finalArr = elementsArr.reduce((acc, element, index) => {
+      if (element === 0) return acc;
+      
+      let newElement = `${element} ${reference[index]}`;
+      if (element !== 1) newElement += 's';
+      acc.push(newElement);
+      return acc;
+    }, []);
+  
+    if (!finalArr.length) return '0 seconds';
+    let finalStr =  finalArr.reverse().join(', ');
+  
+    return finalStr.replace(/,(?=[^,]*$)/, ' and');
+  }
+
+if (typeof module !== 'undefined') {
+  module.exports = { secondsToTimeString };
 }
--- secondsToTimeString/spec/secondsToTimeString.spec.js
+++ secondsToTimeString/spec/secondsToTimeString.spec.js
@@ -1,13 +1,33 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {secondsToTimeString} = require(path.join(__dirname, '..', './secondsToTimeString.js'));
+var { secondsToTimeString } = require(path.join(__dirname, '..', './secondsToTimeString.js'));
 
 describe('secondsToTimeString()', function () {
-  'use strict';
-
-  it('return a string', function () {
-    expect(secondsToTimeString()).to.be.a('string');
+  it('returns "1 second" for input equal 1', function () {
+    expect(secondsToTimeString(1)).to.equal('1 second');
   });
-
+  it('returns "x seconds" for input not equal 1', function () {
+    expect(secondsToTimeString(0)).to.equal('0 seconds');
+    expect(secondsToTimeString(5)).to.equal('5 seconds');
+  });
+  it('returns full minutes', function () {
+    expect(secondsToTimeString(60)).to.equal('1 minute');
+    expect(secondsToTimeString(120)).to.equal('2 minutes');
+  });
+  it('returns minutes and seconds', function () {
+    expect(secondsToTimeString(121)).to.equal('2 minutes and 1 second');
+  });
+  it('returns hours, minutes and seconds', function () {
+    expect(secondsToTimeString(3661)).to.equal('1 hour, 1 minute and 1 second');
+    expect(secondsToTimeString(7261)).to.equal('2 hours, 1 minute and 1 second');
+  });
+  it('returns days, hours, minutes and seconds', function () {
+    expect(secondsToTimeString(90061)).to.equal('1 day, 1 hour, 1 minute and 1 second');
+    expect(secondsToTimeString(176461)).to.equal('2 days, 1 hour, 1 minute and 1 second');
+  });
+  it('returns years, days, hours, minutes and seconds', function () {
+    expect(secondsToTimeString(31626061)).to.equal('1 year, 1 day, 1 hour, 1 minute and 1 second');
+    expect(secondsToTimeString(127702942)).to.equal('4 years, 18 days, 1 hour, 2 minutes and 22 seconds');
+  });
 });
