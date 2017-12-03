ea365bc179651299cf02c70b60f8ed74885f15a2
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-03 21:31:22 +0000

    Add morseCode function + convert a string to morse code + tests

--- morseCode/morseCode.js
+++ morseCode/morseCode.js
@@ -1,8 +1,61 @@
-// Please do not change the name of this function
-var morseDecode = function () {
-  // your code goes here
+var morseDecode = function (str, toMorse) {
+
+  const reference = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0'
+  };
+
+  const refCode = Object.keys(reference);
+  const refChar = Object.values(reference);
+
+  if (toMorse) {
+    return str.split(' ').map((word) => {
+      return word.split('').map((char) => {
+        const charIndex = refChar.indexOf(char);
+        return refCode[charIndex];
+      }).join(' ');
+    }).join('    ');
+  }
+
+  return str.split('    ').map((word) => {
+    return word.split(' ').map((char) => reference[char]).join('');
+  }).join(' ');
 };
 
-if ( typeof module !== 'undefined' ) {
-  module.exports = {morseDecode};
+if (typeof module !== 'undefined') {
+  module.exports = { morseDecode };
 }
--- morseCode/spec/morseCode.spec.js
+++ morseCode/spec/morseCode.spec.js
@@ -4,10 +4,31 @@ var expect = require('chai').expect;
 var {morseDecode} = require(path.join(__dirname, '..', './morseCode.js'));
 
 describe('morseDecode()', function () {
-  'use strict';
-
   it('exists', function () {
     expect(morseDecode).to.be.a('function');
-
+  });
+  it('works for single letter', function () {
+    expect(morseDecode('....')).to.equal('H');
+  });
+  it('works for single word', function () {
+    expect(morseDecode('.... ..')).to.equal('HI');
+    expect(morseDecode('.... . .-.. .-.. ---')).to.equal('HELLO');
+    expect(morseDecode('-. --- .-. - .... -.-. --- -.. . .-. ...')).to.equal('NORTHCODERS'); 
+  });
+  it('works for sentence', function () {
+    const morse = '--. --- --- -..    -- --- .-. -. .. -. --.    -. --- .-. - .... -.-. --- -.. . .-. ...';
+    expect(morseDecode(morse)).to.equal('GOOD MORNING NORTHCODERS');
+  });
+  it('converts single letter to morse code', function () {
+    expect(morseDecode('H', true)).to.equal('....');
+  });
+  it('converts single word to morse code', function () {
+    expect(morseDecode('HI', true)).to.equal('.... ..');
+    expect(morseDecode('HELLO', true)).to.equal('.... . .-.. .-.. ---');
+    expect(morseDecode('NORTHCODERS', true)).to.equal('-. --- .-. - .... -.-. --- -.. . .-. ...'); 
+  });
+  it('converts sentence to morse code', function () {
+    const morse = '--. --- --- -..    -- --- .-. -. .. -. --.    -. --- .-. - .... -.-. --- -.. . .-. ...';
+    expect(morseDecode('GOOD MORNING NORTHCODERS', true)).to.equal(morse);
   });
 });
