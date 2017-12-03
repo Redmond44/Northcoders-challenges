5adf7b82ddfcac76967ce112820733e96c7e63f4
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-03 22:04:41 +0000

    Add changeCalculator + tests

--- changeCalculator/changeCalculator.js
+++ changeCalculator/changeCalculator.js
@@ -1,8 +1,15 @@
-// Please do not change the name of this function
-var changeCalculator = function () {
-  // your code goes here
+var changeCalculator = function (amountOfChange) {
+  const reference = [200, 100, 50, 20, 10, 5, 2, 1];
+
+  return reference.reduce((acc, denomination) => {
+    const value = Math.floor(amountOfChange / denomination);
+    amountOfChange = amountOfChange % denomination;
+    if (denomination > 50) denomination = `£${denomination / 100}`;
+    if (value) acc[denomination] = value;
+    return acc;
+  }, {});
 };
 
-if ( typeof module !== 'undefined' ) {
-  module.exports = {changeCalculator};
+if (typeof module !== 'undefined') {
+  module.exports = { changeCalculator };
 }
--- changeCalculator/spec/changeCalculator.spec.js
+++ changeCalculator/spec/changeCalculator.spec.js
@@ -1,12 +1,29 @@
 var expect = require('chai').expect;
 
-var {changeCalculator} = require('../changeCalculator');
+var { changeCalculator } = require('../changeCalculator');
 
 describe('changeCalculator()', function () {
-  'use strict';
-
-  it('ADD TEST AND RENAME', function () {
-    
+  it('return an empty object if no change', function () {
+    expect(changeCalculator(0)).to.eql({});
   });
-
+  it('return an object with one property if one-coin change', function () {
+    expect(changeCalculator(1)).to.eql({ '1': 1 });
+    expect(changeCalculator(2)).to.eql({ '2': 1 });
+    expect(changeCalculator(5)).to.eql({ '5': 1 });
+  });
+  it('return an object with more properties (but values equal 1)', function () {
+    expect(changeCalculator(3)).to.eql({ '1': 1, '2': 1 });
+    expect(changeCalculator(7)).to.eql({ '5': 1, '2': 1 });
+    expect(changeCalculator(13)).to.eql({ '10': 1, '2': 1, '1': 1 });
+  });
+  it('return an object with more properties', function () {
+    expect(changeCalculator(4)).to.eql({ '2': 2 });
+    expect(changeCalculator(9)).to.eql({ '5': 1, '2': 2 });
+    expect(changeCalculator(99)).to.eql({ '50': 1, '20': 2, '5': 1, '2': 2 });
+  });
+  it('works for £1 and £2', function () {
+    expect(changeCalculator(100)).to.eql({ '£1': 1 });
+    expect(changeCalculator(200)).to.eql({ '£2': 1 });
+    expect(changeCalculator(399)).to.eql({ '£2': 1, '£1': 1, '50': 1, '20': 2, '5': 1, '2': 2 });
+  });
 });
