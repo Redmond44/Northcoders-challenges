6c06d0f3470384efe92d123aace525d7d4c8c159
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-07 09:39:02 +0000

    Add findMissingLetter function + tests

--- findMissingLetter/findMissingLetter.js
+++ findMissingLetter/findMissingLetter.js
@@ -1,6 +1,8 @@
-// Please do not rename this function
-var findMissingLetter = function () {
-  // Your code here
+var findMissingLetter = function (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i].charCodeAt(0) + 1 !== arr[i + 1].charCodeAt(0)) return String.fromCharCode(arr[i].charCodeAt(0) + 1);
+  }
+  return '';
 };
 
 if (typeof module !== 'undefined') {
--- findMissingLetter/spec/findMissingLetter.spec.js
+++ findMissingLetter/spec/findMissingLetter.spec.js
@@ -4,9 +4,20 @@ var expect = require('chai').expect;
 var {findMissingLetter} = require(path.join(__dirname, '..', 'findMissingLetter.js'));
 
 describe('findMissingLetter()', function () {
-  'use strict';
-
   it('exists', function () {
     expect(findMissingLetter).to.be.a('function');
+  });
+  it('returns an empty string if no letter missing', function () {
+    expect(findMissingLetter(['A', 'B'])).to.equal('');
+    expect(findMissingLetter(['A', 'B', 'C', 'D', 'E'])).to.equal('');
+    expect(findMissingLetter(['C', 'D', 'E'])).to.equal('');
+  });
+  it('returns missing letter', function () {
+    expect(findMissingLetter(['A', 'B', 'C', 'E'])).to.equal('D');
+    expect(findMissingLetter(['C', 'E', 'F'])).to.equal('D');    
+  });
+  it('works for small letters', function () {
+    expect(findMissingLetter(['e', 'f', 'g', 'i'])).to.equal('h');
+    expect(findMissingLetter(['e', 'f', 'g', 'h', 'i'])).to.equal('');    
   });
 });
