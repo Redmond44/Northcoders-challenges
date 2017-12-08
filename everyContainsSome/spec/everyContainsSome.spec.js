bce8313e333d5200b94a846e64794f20e2804e9e
Harriet <harriethryder@gmail.com>
2017-12-08 05:00:45 +0000

    Katapult everyContainsSome

---
+++ everyContainsSome/spec/everyContainsSome.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {everyContainsSomeTest} = require(path.join(__dirname, '..', './everyContainsSome.js'));
+
+describe('everyContainsSomeTest()', function () {
+  'use strict';
+
+});
