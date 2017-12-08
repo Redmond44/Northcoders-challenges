64c0e2e07ba756a5863e683237f7650bffe698d6
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-08 09:31:01 +0000

    Add every - returns true if non-list argument

--- everyContainsSome/spec/everyContainsSome.spec.js
+++ everyContainsSome/spec/everyContainsSome.spec.js
@@ -7,4 +7,12 @@ describe('everyContainsSomeTest()', function () {
   it('returns true if no arguments', () => {
     expect(everyContainsSomeTest.every()).to.equal(true);
   });
+  it('returns true if non-list argument given (no predicate)', () => {
+    expect(everyContainsSomeTest.every(null)).to.equal(true);
+    expect(everyContainsSomeTest.every(undefined)).to.equal(true);
+    expect(everyContainsSomeTest.every(true)).to.equal(true);
+    expect(everyContainsSomeTest.every(false)).to.equal(true);
+    expect(everyContainsSomeTest.every(23)).to.equal(true);
+    expect(everyContainsSomeTest.every(0)).to.equal(true);
+  });
 });
