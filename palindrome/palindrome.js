c398647cebe9651a20cd68bcd9ad35faa7f93de9
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-12 09:30:13 +0000

    Add palindrome function + tests

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,8 +1,16 @@
-// Please do not change the name of this function
-var palindrome = function () {
+var palindrome = function (str) {
+  // const newStr = str.replace(/\s/g, '').toLowerCase();
+  // return newStr === newStr.split('').reverse().join('');
 
+  // recursive solution
+  const newStr = str.replace(/\s/g, '');
+
+  if (newStr.length < 2) return true;
+  if (newStr[0].toLowerCase() !== newStr[newStr.length - 1].toLowerCase())
+     return false;    
+  return palindrome(newStr.slice(1, -1));
 };
 
-if ( typeof module !== 'undefined' ) {
-  module.exports = {palindrome};
+if (typeof module !== 'undefined') {
+  module.exports = { palindrome };
 }
--- palindrome/spec/palindrome.spec.js
+++ palindrome/spec/palindrome.spec.js
@@ -1,9 +1,29 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {palindrome} = require(path.join(__dirname, '..', './palindrome.js'));
+var { palindrome } = require(path.join(__dirname, '..', './palindrome.js'));
 
 describe('palindrome()', function () {
-  'use strict';
-
+  it('returns true for an empty string', () => {
+    expect(palindrome('')).to.equal(true);
+  });
+  it('returns true for single letter', () => {
+    expect(palindrome('a')).to.equal(true);
+    expect(palindrome('B')).to.equal(true);
+  });
+  it('works for single word', () => {
+    expect(palindrome('cat')).to.equal(false);
+    expect(palindrome('cahgc')).to.equal(false);
+    expect(palindrome('cahac')).to.equal(true);
+  });
+  it('works for sentence', () => {
+    expect(palindrome('acrobats stab orca')).to.equal(true);
+    expect(palindrome('are we not drawn onward to new era')).to.equal(true);    
+    expect(palindrome('acrobats stab')).to.equal(false);
+  });
+  it('insensitive to capitals', () => {
+    expect(palindrome('Acrobats stab orca')).to.equal(true);
+    expect(palindrome('Are we not drawn onward to new era')).to.equal(true);    
+    expect(palindrome('Acrobats stab')).to.equal(false);
+  });
 });
