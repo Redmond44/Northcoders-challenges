8aaaa31e17b1706dcf30e447fca711ac504732ef
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-15 08:54:25 +0000

    Add sumConsecutives and reduceConsecutives functions + tests

--- sumConsecutives/spec/sumConsecutives.spec.js
+++ sumConsecutives/spec/sumConsecutives.spec.js
@@ -1,9 +1,29 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {sumConsecutives} = require(path.join(__dirname, '..', './sumConsecutives.js'));
+var { sumConsecutives, reduceConsecutives } = require(path.join(__dirname, '..', './sumConsecutives.js'));
 
 describe('sumConsecutives()', function () {
-  'use strict';
+  it('returns new array', () => {
+    const arr = [1, 1, 1];
+    sumConsecutives(arr);
+    expect(arr).to.eql([1, 1, 1]);
+  });
+  it('returns exact copy of an array if no consecutive numbers', () => {
+    expect(sumConsecutives([1, 2, 3])).to.eql([1, 2, 3]);
+  });
+  it('sums all numbers if all are consecutive', () => {
+    expect(sumConsecutives([1, 1, 1])).to.eql([3]);
+  });
+  it('sums all the consecutive numbers in an array and pushes them into a new array', () => {
+    expect(sumConsecutives([1, 1, 2, 3])).to.eql([2, 2, 3]);
+    expect(sumConsecutives([1, 1, 2, 2, 3, 3])).to.eql([2, 4, 6]);
+    expect(sumConsecutives([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1])).to.eql([2, 2, 4, 2, 3]);
+  });
+});
 
+describe('reduceConsecutives()', function () {
+  it('reduces the array down to a single value made off adding the array elements together once the array has no consecutive numbers', () => {
+    expect(reduceConsecutives([1,1,2,1,1,1,1,2,1,1,1])).to.eql([8, 2, 3]);
+  });
 });
--- sumConsecutives/sumConsecutives.js
+++ sumConsecutives/sumConsecutives.js
@@ -1,8 +1,28 @@
-// Please do not change the name of this function
-var sumConsecutives = function () {
-  // your code goes here
+var sumConsecutives = function (arr) {
+  let newArr = [];
+  let currentNum = arr[0];
+  let sum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === currentNum) sum += arr[i];
+    else {
+      currentNum = arr[i];
+      newArr.push(sum);
+      sum = arr[i];
+    }
+    if (i === arr.length - 1) newArr.push(sum);
+  }
+
+  return newArr;
 };
 
+function reduceConsecutives(arr) {
+  const finalArr = sumConsecutives(arr);
+
+  if (finalArr.length === arr.length) return finalArr;
+  return reduceConsecutives(finalArr);
+}
+
 if (typeof module !== 'undefined') {
-  module.exports = {sumConsecutives};
+  module.exports = { sumConsecutives, reduceConsecutives };
 }
