1879bc73bc0ccf24e35280acf799b8c38f49e9ca
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-15 09:17:26 +0000

    Add alphabetReplace function + tests

--- alphabetReplace/alphabetReplace.js
+++ alphabetReplace/alphabetReplace.js
@@ -1,6 +1,10 @@
-// Please do not change the name of this function
 function alphabetReplace(str) {
-  
+  const positionZero = 'a'.charCodeAt(0) - 1;
+  str = str.replace(/ /g, '').toLowerCase();
+
+  return str.split('').map(char => {
+    return char.charCodeAt(0) - positionZero;
+  }).join(' ');
 }
 
 module.exports = {alphabetReplace};
--- alphabetReplace/spec/alphabetReplace.spec.js
+++ alphabetReplace/spec/alphabetReplace.spec.js
@@ -2,5 +2,16 @@ const { expect } = require('chai');
 const {alphabetReplace} = require('../alphabetReplace');
 
 describe('alphabetReplae', () => {
-  
+  it('returns corresponding index for single letter', () => {
+    expect(alphabetReplace('a')).to.equal('1');
+  });
+  it('returns corresponding indexes for single word', () => {
+    expect(alphabetReplace('code')).to.equal('3 15 4 5');
+  });
+  it('not case sensitive', () => {
+    expect(alphabetReplace('Northcoders')).to.equal('14 15 18 20 8 3 15 4 5 18 19');
+  });
+  it('returns corresponding indexes for sentence', () => {
+    expect(alphabetReplace('expert programming')).to.equal('5 24 16 5 18 20 16 18 15 7 18 1 13 13 9 14 7');
+  });
 });
