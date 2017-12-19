86d305341aaea6087450ef12852a1efea0602770
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-19 08:49:31 +0000

    Add foldArray function + tests

--- foldArray/foldArray.js
+++ foldArray/foldArray.js
@@ -1,6 +1,15 @@
-// Please do not rename this function
-function foldArray () {
+function foldArray(arr, num) {
+  if (!num) return arr;
 
+  const middle = arr.length / 2 - 1;
+  const newArr = [];
+
+  for (let i = 0; i <= middle; i++) {
+    newArr.push(arr[i] + arr[arr.length - 1 - i]);
+  }
+  if (middle !== Math.floor(middle)) newArr.push(arr[Math.floor(middle) + 1]);
+
+  return foldArray(newArr, num - 1);
 }
 
-module.exports = {foldArray};
+module.exports = { foldArray };
--- foldArray/spec/foldArray.spec.js
+++ foldArray/spec/foldArray.spec.js
@@ -1,6 +1,27 @@
 const expect = require('chai').expect;
-const {foldArray} = require('../foldArray');
+const { foldArray } = require('../foldArray');
 
 describe('foldArray', function () {
-
+  it('returns the same array if second argument is not given', () => {
+    expect(foldArray([1, 2, 3])).to.eql([1, 2, 3]);
+  });
+  it('returns an empty array for an empty array', () => {
+    expect(foldArray([], 1)).to.eql([], 1);
+  });
+  it('returns the same array if one element', () => {
+    expect(foldArray([1], 1)).to.eql([1]);
+  });
+  it('folds all integers if the array has an even length', () => {
+    expect(foldArray([1, 2], 1)).to.eql([3]);
+    expect(foldArray([1, 2, 3, 10, 34, 100], 1)).to.eql([101, 36, 13]);
+  });
+  it('fold on the middle index if the array has an odd length', () => {
+    expect(foldArray([1, 2, 3], 1)).to.eql([4, 2]);
+  });
+  it('works for num greater than one', () => {
+    expect(foldArray([1, 2, 3, 10, 34, 100], 2)).to.eql([114, 36]);
+    expect(foldArray([1, 2, 3, 10, 34, 100], 3)).to.eql([150]);
+    expect(foldArray([1, 2, 3, 10, 34, 100], 4)).to.eql([150]);
+    expect(foldArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)).to.eql([15, 20, 10]);
+  });
 });
