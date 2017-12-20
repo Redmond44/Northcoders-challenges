5f8b8c627eb1ba495e3539f218bcbfc06c05fd18
dzewelina <ewelina.zukowska26@gmail.com>
2017-12-20 08:48:42 +0000

    Replace splice with slice

--- supermarketQueue/supermarketQueue.js
+++ supermarketQueue/supermarketQueue.js
@@ -1,7 +1,8 @@
 function queueTime(arr, num) {
-  const checkoutTills = arr.splice(0, num);
+  // slice better than splice - doesn't change original array
+  const checkoutTills = arr.slice(0, num);
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = num; i < arr.length; i++) {
     let minTime = Math.min(...checkoutTills);
     let minTimeIndex = checkoutTills.indexOf(minTime);
     checkoutTills[minTimeIndex] += arr[i];
