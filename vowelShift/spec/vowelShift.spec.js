22bcc1526790edb7ce9c9b7a24776954eff26ee6
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-01 18:16:44 +0000

    Add vowelShift function + tests

--- vowelShift/spec/vowelShift.spec.js
+++ vowelShift/spec/vowelShift.spec.js
@@ -1,9 +1,19 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {vowelShift} = require(path.join(__dirname, '..', './vowelShift.js'));
+var { vowelShift } = require(path.join(__dirname, '..', './vowelShift.js'));
 
 describe('vowelShift()', function () {
-  'use strict';
-
+  it('returns unchanged string if no second argument or equal 0', () => {
+    expect(vowelShift('foo')).to.eql('foo');
+    expect(vowelShift('foo', 0)).to.eql('foo');
+  });
+  it('shifts vowels 1 vowel position to the right if second argument equal 1', () => {
+    expect(vowelShift('hello', 1)).to.eql('holle');
+    expect(vowelShift('hello child', 1)).to.eql('hille chold');
+  });
+  it('shifts every vowel in the string n vowel positions', () => {
+    expect(vowelShift('star nosed mole', 2)).to.eql('stor nesad mole');
+    expect(vowelShift('funnily enough', 4)).to.eql('finnely onuugh');
+  });
 });
--- vowelShift/vowelShift.js
+++ vowelShift/vowelShift.js
@@ -1,8 +1,33 @@
-// Please do not change the name of this function
-var vowelShift = function (str, n) {
-  
+const vowelShift = function (str, n) {
+  const strArray = str.split('');
+  const refVowels = ['a', 'e', 'i', 'o', 'u'];
+  let vowelsInStr = {};
+
+  strArray.forEach((char, index) => {
+    if (refVowels.includes(char)) vowelsInStr[index] = char;
+  });
+
+  const vowelsPositions = Object.keys(vowelsInStr);
+  const vowels = Object.values(vowelsInStr);
+  const newVowelsOrder = rotateArray(vowels, n);
+
+  vowelsPositions.forEach((position, i) => {
+    strArray[position] = newVowelsOrder[i];
+  });
+
+  return strArray.join('');
+};
+
+const rotateArray = function (arr, num) {
+  let newStartIndex;
+
+  if (num >= 0) newStartIndex = arr.length - num;
+  if (num < 0) newStartIndex = Math.abs(num);
+
+  let newArr = arr.slice(newStartIndex).concat(arr.slice(0, newStartIndex));
+  return newArr;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {vowelShift};
+  module.exports = { vowelShift };
 }
