fac20729554f4db29b104c32d184637b2e7c2edb
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-01 18:15:26 +0000

    Add romanNumeralEncoder function + tests

--- romanNumeralEncoder/romanNumeralEncoder.js
+++ romanNumeralEncoder/romanNumeralEncoder.js
@@ -1,8 +1,31 @@
-// Please do not change the name of this function
-var romanNumeralEncoder = function () {
-  // Your code here
+const romanNumeralEncoder = function (num) {
+  if (typeof num !== 'number') return '';
+  const ref = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+  let roman = '';
+
+  for (let char in ref) {
+    while (num >= ref[char]) {
+      roman += char;
+      num -= ref[char];
+    }
+  }
+  return roman;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {romanNumeralEncoder};
+  module.exports = { romanNumeralEncoder };
 }
--- romanNumeralEncoder/spec/romanNumeralEncoder.spec.js
+++ romanNumeralEncoder/spec/romanNumeralEncoder.spec.js
@@ -1,9 +1,20 @@
 var path = require('path');
 var expect = require('chai').expect;
 
-var {romanNumeralEncoder} = require(path.join(__dirname, '..', './romanNumeralEncoder.js'));
+var { romanNumeralEncoder } = require(path.join(__dirname, '..', './romanNumeralEncoder.js'));
 
 describe('romanNumeralEncoder()', function () {
-  'use strict';
-
+  it('returns an empty string for invalid input', () => {
+    expect(romanNumeralEncoder(true)).to.equal('');
+    expect(romanNumeralEncoder('cat')).to.equal('');
+  });
+  it('returns a Roman Numeral string when passed a number', () => {
+    expect(romanNumeralEncoder(1)).to.equal('I');
+    expect(romanNumeralEncoder(2)).to.equal('II');
+    expect(romanNumeralEncoder(10)).to.equal('X');
+    expect(romanNumeralEncoder(17)).to.equal('XVII');
+    expect(romanNumeralEncoder(60)).to.equal('LX');
+    expect(romanNumeralEncoder(75)).to.equal('LXXV');
+    expect(romanNumeralEncoder(91)).to.equal('XCI');
+  });
 });
