7e636611a8e2f265a526945c1aa7a67e6aaffe4a
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-04 09:50:32 +0000

    Add findMostRepeated function + tests

--- findMostRepeated/findMostRepeated.js
+++ findMostRepeated/findMostRepeated.js
@@ -1,8 +1,18 @@
-// Please do not change the name of this function
-var findMostRepeated = function (arr) {
-  
+const findMostRepeated = function (arr) {
+  const countingObj = {};
+
+  arr.forEach(element => {
+    if (!countingObj[element]) countingObj[element] = 1;
+    else countingObj[element]++;
+  });
+
+  let repeats = Math.max(...Object.values(countingObj));
+  if (repeats < 2) repeats = null;
+  const elements = Object.keys(countingObj).filter(key => countingObj[key] === repeats);
+
+  return { elements, repeats };
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {findMostRepeated};
+  module.exports = { findMostRepeated };
 }
--- findMostRepeated/spec/findMostRepeated.spec.js
+++ findMostRepeated/spec/findMostRepeated.spec.js
@@ -1,8 +1,19 @@
-var path = require('path');
-var expect = require('chai').expect;
+const path = require('path');
+const expect = require('chai').expect;
 
-var {findMostRepeated} = require(path.join(__dirname, '..', './findMostRepeated.js'));
+const { findMostRepeated } = require(path.join(__dirname, '..', './findMostRepeated.js'));
 
 describe('findMostRepeated()', function () {
-  'use strict';
+  it('returns an object with empty array of elements and null for repeats if the array is empty', () => {
+    expect(findMostRepeated([])).to.eql({ elements: [], repeats: null });
+  });
+  it('returns an object with empty array of elements and null for repeats if there are no repeated elements', () => {
+    expect(findMostRepeated(['foo', 'bar', 'hello', 'world'])).to.eql({ elements: [], repeats: null });
+  });
+  it('returns an object that describes the most repeated single element in the array', () => {
+    expect(findMostRepeated(['foo', 'foo', 'bar', 'hello', 'world'])).to.eql({ elements: ['foo'], repeats: 2 });
+  });
+  it('returns an object that describes the most repeated elements in the array', () => {
+    expect(findMostRepeated(['foo', 'foo', 1, 2, 3, 'bar', 2, 3, 4, 'bar', 'bar', 'foo'])).to.eql({ elements: ['foo', 'bar'], repeats: 3 });
+  });
 });
