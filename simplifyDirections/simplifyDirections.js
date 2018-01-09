bf6242d5af051fed3db847ea62f4abb18bace3f1
Harriet <harriethryder@gmail.com>
2018-01-09 05:00:45 +0000

    Katapult simplifyDirections

---
+++ simplifyDirections/simplifyDirections.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var simplifyDirections = function (arr) {
+  // YOUR CODE HERE
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {simplifyDirections};
+}
---
+++ simplifyDirections/spec/simplifyDirections.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {simplifyDirections} = require(path.join(__dirname, '..', './simplifyDirections.js'));
+
+describe('simplifyDirections()', function () {
+  'use strict';
+
+});
