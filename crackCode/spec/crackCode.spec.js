b4416dd8020d73cf701a3d5fe8b5d012ab509849
Harriet <harriethryder@gmail.com>
2018-01-10 05:00:44 +0000

    Katapult crackCode

---
+++ crackCode/spec/crackCode.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {crackCode} = require(path.join(__dirname, '..', 'crackCode.js'));
+
+describe('crackCode()', function () {
+  'use strict';
+
+});
