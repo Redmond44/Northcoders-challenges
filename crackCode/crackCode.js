d11a1ecf2aa56b9a92b1244f0e3c48959d8b5727
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-13 20:44:14 +0000

    Add crackCode function + tests

--- crackCode/crackCode.js
+++ crackCode/crackCode.js
@@ -1,8 +1,45 @@
-// Please do not rename this function
-var crackCode = function (code) {
-  // Your code here
+const crackCode = function (code) {
+  const regex = /^(([a-z]+-)+[a-z]+)(-\d+)*\((\w+)\)$/;
+
+  // preliminary check
+  const bracketsCode = code.match(regex)[4];
+  if (bracketsCode !== bracketsCode.split('').sort().join(''))
+    return false;
+
+  // MAIN CHECK
+  const outsideBracketsCode = code.match(regex)[1].split('-');
+
+  // specify the number of occurrences of each letter
+  const lettersCounter = outsideBracketsCode.reduce((acc, element) => {
+    if (acc[element.length])
+      acc[element.length] += element[0];
+    else
+      acc[element.length] = element[0];
+
+    return acc;
+  }, {});
+
+  // find the greatest occurrence
+  const keys = Object.keys(lettersCounter);
+  const theMostCommon = Math.max(...keys);
+
+  // construct the correct code
+  let matchingCode = '';
+  let i = theMostCommon;
+
+  do {
+    if (lettersCounter[i])
+      matchingCode += lettersCounter[i].split('').sort().join('');
+    i--;
+  } while (matchingCode.length < 4);
+
+  matchingCode = matchingCode.slice(0, 4).split('').sort().join('');
+
+  if (matchingCode === bracketsCode) return true;
+
+  return false;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {crackCode};
+  module.exports = { crackCode };
 }
--- crackCode/spec/crackCode.spec.js
+++ crackCode/spec/crackCode.spec.js
@@ -1,9 +1,24 @@
-var path = require('path');
-var expect = require('chai').expect;
+const path = require('path');
+const expect = require('chai').expect;
 
-var {crackCode} = require(path.join(__dirname, '..', 'crackCode.js'));
+const { crackCode } = require(path.join(__dirname, '..', 'crackCode.js'));
 
 describe('crackCode()', function () {
-  'use strict';
-
+  it('returns false if code in brackets is not in the alphabetical order', () => {
+    expect(crackCode('a-b-c-d-123(acbd)')).to.equal(false);
+  });
+  it('returns false if letters in bracket-code are not the most common', () => {
+    expect(crackCode('fff-gg-xx-ss-y(fgsy)')).to.equal(false);
+  });
+  it('returns false if letters in bracket-code are not alphabetically first', () => {
+    expect(crackCode('a-b-c-d-e-f-g-h-i-577(efgh)')).to.equal(false);
+    expect(crackCode('bbb-yy-a-cc-zzz-ee(bcez)')).to.equal(true);
+  });
+  it('works correctly for random occurrence of letters', () => {
+    expect(crackCode('ddd-aaa-z-y-x-123(adxy)')).to.equal(true);
+  });
+  it('returns true for valid keys ', () => {
+    expect(crackCode('a-b-c-d-e-f-g-h-i-577(abcd)')).to.equal(true);
+    expect(crackCode('fff-gg-xx-ss-y(fgsx)')).to.equal(true);
+  });
 });
