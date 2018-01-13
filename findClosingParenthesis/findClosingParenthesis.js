c97cbb9655e844069534801b83c7d910cb956e98
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-13 18:52:37 +0000

    Add findClosingParenthesis function + tests

--- findClosingParenthesis/findClosingParenthesis.js
+++ findClosingParenthesis/findClosingParenthesis.js
@@ -1,8 +1,24 @@
-// Please do not change the name of this function
-var findClosingParenthesis = function (str, n) {
-  
+const findClosingParenthesis = function (str, n) {
+  let number = 0;
+  const refObj = {
+    '(': 0,
+    ')': 0
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' && number !== n) number++;
+
+    if (number === n) {
+      if (str[i] === '(') refObj['(']++;
+      if (str[i] === ')') refObj[')']++;
+
+      if (refObj['('] === refObj[')']) return i;
+    }
+  }
+
+  return -1;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {findClosingParenthesis};
+  module.exports = { findClosingParenthesis };
 }
--- findClosingParenthesis/spec/findClosingParenthesis.spec.js
+++ findClosingParenthesis/spec/findClosingParenthesis.spec.js
@@ -1,9 +1,22 @@
-var path = require('path');
-var expect = require('chai').expect;
+const path = require('path');
+const expect = require('chai').expect;
 
-var {findClosingParenthesis} = require(path.join(__dirname, '..', './findClosingParenthesis.js'));
+const { findClosingParenthesis } = require(path.join(__dirname, '..', './findClosingParenthesis.js'));
 
 describe('findClosingParenthesis()', function () {
-  'use strict';
-  
+  it('returns "-1" if second argument is not given', () => {
+    expect(findClosingParenthesis('(hello)')).to.equal(-1);
+  });
+  it('returns "-1" if number is bigger than number of parentheses', () => {
+    expect(findClosingParenthesis('hello', 1)).to.equal(-1);
+  });
+  it('works for single parenthesis', () => {
+    expect(findClosingParenthesis('(hello)', 1)).to.equal(6);
+  });
+  it('works for a few parentheses', () => {
+    expect(findClosingParenthesis('Hello, (world). (Something Else).', 2)).to.equal(31);
+  });
+  it('works for nested parentheses', () => {
+    expect(findClosingParenthesis('Hello, (world, (foo) bar (something) else), foo (bar) cat', 3)).to.equal(35);
+  });
 });
