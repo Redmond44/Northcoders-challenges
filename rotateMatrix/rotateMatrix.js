386f5974c506bcdab7b4b46d28f651ac7a5a40dc
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-16 19:49:22 +0000

    Add rotateMatrix function + tests

--- rotateMatrix/rotateMatrix.js
+++ rotateMatrix/rotateMatrix.js
@@ -1,8 +1,22 @@
-// Please do not change the name of this function
-function rotateMatrix (matrix) {
-    // YOUR CODE HERE
+function rotateMatrix(matrix) {
+  const numOfNestedArr = matrix.length;
+  let arrLength = 0;
+  if (numOfNestedArr) arrLength = matrix[0].length;
+
+  const result = [];
+
+  for (let i = 0; i < arrLength; i++) {
+    // add an empty nested array
+    result.push([]);
+    for (let j = 0; j < numOfNestedArr; j++) {
+      // fill nested array
+      result[i][j] = matrix[numOfNestedArr - 1 - j][i];
+    }
+  }
+
+  return result;
 }
 
 if (typeof module !== 'undefined') {
-  module.exports = {rotateMatrix};
+  module.exports = { rotateMatrix };
 }
--- rotateMatrix/spec/rotateMatrix.spec.js
+++ rotateMatrix/spec/rotateMatrix.spec.js
@@ -1,9 +1,45 @@
-var path = require('path');
-var expect = require('chai').expect;
+const path = require('path');
+const expect = require('chai').expect;
 
-var {rotateMatrix} = require(path.join(__dirname, '..', './rotateMatrix.js'));
-
+const { rotateMatrix } = require(path.join(__dirname, '..', './rotateMatrix.js'));
 
 describe('rotateMatrix', function () {
-
+  it('returns an empty array if an empty array given', () => {
+    expect(rotateMatrix([])).to.eql([]);
+  });
+  it('returns unchanged array for one nested array with single element', () => {
+    expect(rotateMatrix([[3]])).to.eql([[3]]);
+  });
+  it('returns two nested arrays for one nested array with two elements', () => {
+    expect(rotateMatrix([[3, 4]])).to.eql([[3], [4]]);
+  });
+  it('works for square matrix', () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+    const expected = [
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3]
+    ];
+    expect(rotateMatrix(matrix)).to.eql(expected);
+  });
+  it('works for rectangular matrix', () => {
+    const matrix = [
+      [1, 2, 3, 5, 10, 1],
+      [3, 3, 1, 1, 3, 6],
+      [1, 6, 3, 2, 2, 1]
+    ];
+    const expected = [
+      [1, 3, 1],
+      [6, 3, 2],
+      [3, 1, 3],
+      [2, 1, 5],
+      [2, 3, 10],
+      [1, 6, 1]
+    ];
+    expect(rotateMatrix(matrix)).to.eql(expected);
+  });
 });
