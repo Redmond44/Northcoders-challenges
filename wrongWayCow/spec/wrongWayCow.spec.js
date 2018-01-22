3356d2e5356cb0a75e64bdd6481c05feae29b22e
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-22 21:07:23 +0000

    Add wrongWayCow function + tests

--- wrongWayCow/spec/wrongWayCow.spec.js
+++ wrongWayCow/spec/wrongWayCow.spec.js
@@ -2,5 +2,23 @@ const {expect} = require('chai');
 const {wrongWayCow} = require('../wrongWayCow');
 
 describe('wrongWayCow', function () {
-  
+  it('returns an empty array if all cows are facing the right way', () => {
+    expect(wrongWayCow('cowcowcowcow')).to.eql([]);
+    expect(wrongWayCow('wocwocwoc')).to.eql([]);
+  });
+  it('returns the coordinates of the cow facing the wrong way for single line of cows', () => {
+    expect(wrongWayCow('cowwoccowcow')).to.eql([0, 1]);
+    expect(wrongWayCow('wocwocwoccow')).to.eql([0, 3]);
+  });
+  it('returns the coordinates of the cow facing the wrong way for few lines of cows', () => {
+    expect(wrongWayCow('cowwoccowcow\ncowcowcowcow')).to.eql([0, 1]);
+    expect(wrongWayCow('cowcowcowcow\ncowcowcowcow')).to.eql([]);
+    expect(wrongWayCow('wocwocwocwoc\nwocwocwoccow\nwocwocwocwoc')).to.eql([1, 3]);
+  });
+  it('returns the coordinates of the cow facing the wrong way if empty pasture', () => {
+    expect(wrongWayCow('cowcowcow...\n...woccowcow\n............')).to.eql([1, 1]);
+    expect(wrongWayCow('cowcowcowcow\n......cowcow\n............\n...cow...woc')).to.eql([3, 3]);
+    expect(wrongWayCow('wocwoc...woc...\n...woc...cow...\n...wocwocwocwoc')).to.eql([1, 3]);
+    expect(wrongWayCow('cowcowcow...\n...cowcowcow\n............')).to.eql([]);
+  });
 });
--- wrongWayCow/wrongWayCow.js
+++ wrongWayCow/wrongWayCow.js
@@ -1,6 +1,29 @@
-function wrongWayCow () {
+function wrongWayCow(str) {
+  let test = [];
+  let index = 0;
+  let pasture;
 
+  const arrOfPastures = str.split('\n');
+
+  for (let j = 0; j < arrOfPastures.length; j++) {
+    for (let i = 0; i < arrOfPastures[j].length; i += 3) {
+      if (arrOfPastures[j][i] === '.') continue;
+      if (!test.includes(arrOfPastures[j][i])) test.push(arrOfPastures[j][i]);
+      else {
+        if (arrOfPastures[j][i] === 'c') index = arrOfPastures[j].indexOf('woc');
+        else if (arrOfPastures[j][i] === 'w') index = arrOfPastures[j].indexOf('cow');
+        if (index > 0) {
+          pasture = j;
+          break;
+        }
+      }
+    }
+    if (pasture !== undefined) break;
+  }
+  
+  if (index < 0) return [];
+  return [pasture, index / 3];
 }
 
 
-module.exports = {wrongWayCow};
+module.exports = { wrongWayCow };
