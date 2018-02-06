2763eef8c6011960b6760d398ccb155d1c1d6ccc
dzewelina <ewelina.zukowska26@gmail.com>
2018-02-06 12:13:40 +0000

    Add simplifyDirections function + tests

--- simplifyDirections/simplifyDirections.js
+++ simplifyDirections/simplifyDirections.js
@@ -1,8 +1,23 @@
-// Please do not change the name of this function
-var simplifyDirections = function (arr) {
-  // YOUR CODE HERE
+const simplifyDirections = function (arr) {
+  const result = [];
+  const refObj = {
+    NORTH: 'SOUTH',
+    SOUTH: 'NORTH',
+    WEST: 'EAST',
+    EAST: 'WEST'
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.includes(refObj[arr[i]])) {
+      const index = arr.indexOf(refObj[arr[i]]);
+      arr.splice(index, 1);
+    }
+    else result.push(arr[i]);
+  }
+
+  return result;
 };
 
 if (typeof module !== 'undefined') {
-  module.exports = {simplifyDirections};
+  module.exports = { simplifyDirections };
 }
--- simplifyDirections/spec/simplifyDirections.spec.js
+++ simplifyDirections/spec/simplifyDirections.spec.js
@@ -1,9 +1,18 @@
-var path = require('path');
-var expect = require('chai').expect;
+const path = require('path');
+const expect = require('chai').expect;
 
-var {simplifyDirections} = require(path.join(__dirname, '..', './simplifyDirections.js'));
+const { simplifyDirections } = require(path.join(__dirname, '..', './simplifyDirections.js'));
 
 describe('simplifyDirections()', function () {
-  'use strict';
-
+  it('returns the same array if no directions to simplyfy', () => {
+    expect(simplifyDirections(['NORTH', 'EAST'])).to.eql(['NORTH', 'EAST']);
+  });
+  it('returns an empty array if the directions cancel one another out', () => {
+    expect(simplifyDirections(['NORTH', 'EAST'])).to.eql(['NORTH', 'EAST']);
+  });
+  it('returns an array with simplified directions', () => {
+    expect(simplifyDirections(['NORTH', 'SOUTH', 'WEST'])).to.eql(['WEST']);
+    expect(simplifyDirections(['NORTH', 'WEST', 'SOUTH', 'WEST', 'EAST'])).to.eql(['WEST']);
+    expect(simplifyDirections(['NORTH', 'EAST', 'SOUTH', 'WEST', 'NORTH', 'NORTH'])).to.eql(['NORTH', 'NORTH']);
+  });
 });
